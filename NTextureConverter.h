0235b20285b46831ec6a1aca39e151ddd72255df
晴彦 相川 <k021g1126@g.neec.ac.jp>
2023-10-16 15:11:09 +0900

    02_05完了

--- NTextureConverter.h
+++ NTextureConverter.h
@@ -1,8 +1,24 @@
 #pragma once
+#include <d3d12.h>
+#include <DirectXTex.h>
+
 #include <string>
 
 class NTextureConverter
 {
+private:
+	//画像の情報
+	DirectX::TexMetadata metadata_;
+	//画像イメージのコンテナ
+	DirectX::ScratchImage scratchImage_;
+
+	//ディレクトリパス
+	std::wstring directoryPath_;
+	//ファイル名
+	std::wstring fileName_;
+	//ファイル拡張子
+	std::wstring fileExt_;
+
 public:
 	/// <summary>
 	/// テクスチャをWICからDDSに変換する
@@ -23,4 +39,15 @@ private:
 	/// <param name="mString"></param>
 	/// <returns>ワイド文字列</returns>
 	static std::wstring ConvertMultiByteStringToWideString(const std::string& mString);
+
+	/// <summary>
+	/// フォルダパスとファイル名を分離する
+	/// </summary>
+	/// <param name="filePath">ファイルパス</param>
+	void SeparateFilePath(const std::wstring& filePath);
+
+	/// <summary>
+	/// DDSテクスチャとしてファイル書き出し
+	/// </summary>
+	void SaveDDSTextureToFile();
 };
